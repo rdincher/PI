ae0b8d5803137a5bbb539d4b840b25ce46ba46ed
rdincher <Ryan@Ryans-MacBook-Air.local>
2017-07-18 22:25:52 -0500

    Add Three Day
    Added Button and function to get three day forecast.
    
    Adding weatherThreeDay.php to return three day forecast

--- Weather/weather.php
+++ Weather/weather.php
@@ -16,7 +16,18 @@
   		});
   		return false;
 	}
-
+	function threeDay() {
+   		$.ajax({
+      		url:'weatherThreeDay.php',
+      		complete: function (response) {
+          		$('#output').html(response.responseText);
+      		},
+      		error: function () {
+         		$('#output').html('Bummer: there was an error!');
+     		}
+  		});
+  		return false;
+	}
   </script>
   	<style>
   		table {
@@ -27,9 +38,7 @@
   <body>
   	<h4> Weather</h4>
   	<button type="button" id="btnTen" onClick=tenDay()>10 Day Forecast</button>
-  	<!--<script>
-  	<document.getElementById ("btnTen").addEventListener ("click", tenDay());
-  	</script>-->
+	<button type="button" id="btnThree" onClick=threeDay()>3 Day Forecast</button>
   	<div id="output">waiting for action</div>
 
   </body>
