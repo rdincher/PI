37016e15cd9dfa3747c10daf7fcaab9c538879b5
rdincher <Ryan@Ryans-MacBook-Air.local>
2018-05-06 14:50:03 -0500

    Rearchitect index
    Split SQL Connection code to a temporary file that can be played with
    later.
    Updated index to provide access to other features of Server.

--- index.php
+++ index.php
@@ -1,58 +1,45 @@
 <! DOCTYPE html>
 <html>
-  <head>  </head>
+  <head>
+    <title>Pi</title>
+  	<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
+  	<script>
+	function weather() {
+   		$.ajax({
+      		url:'weather.php',
+      		complete: function (response) {
+          		$('#output').html(response.responseText);
+      		},
+      		error: function () {
+         		$('#output').html('Bummer: there was an error!');
+     		}
+  		});
+  		return false;
+	}
+	function packing() {
+   		$.ajax({
+      		url:'packing.php',
+      		complete: function (response) {
+          		$('#output').html(response.responseText);
+      		},
+      		error: function () {
+         		$('#output').html('Bummer: there was an error!');
+     		}
+  		});
+  		return false;
+	}
+  </script>
+  	<style>
+  		table {
+    		border-spacing: 15px;
+			}
+ 	 </style>
+  </head>
   <body>
   	<h4> Pi Page</h4>
-  	<form method="post" action"<?php echo $_SERVER['PHP_SELF'];?>">
-  		Name: <input type="text" name="fname">
-  		<input type="submit">
-  	</form>
- 	<?php
- 		
- 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   		 // collect value of input field
-   		 $name = $_REQUEST['fname'];
- 	
-		 $servername = "localhost";
-		 $username = "root";
-		 #$password = "rasberrypi";
-		 $dbname = "website";
-		 
-		 // Create connection
-		 $conn = new mysqli($servername, $username, 'raspberrypi', $dbname);
-		 // Check connection
-		 if (mysqli_connect_error()) {
-    	 	die("Database connection failed: " . mysqli_connect_error());
-		 } 
-		 echo "<br>Connected<br>";
-		 $curDate = date('Y-m-d H:i:s');
-
-		 $loc = $_SERVER['REMOTE_ADDR'];
-
-		 $sql = "INSERT INTO log (name, time, visit) VALUES ('$name', '$curDate', '$loc')";
-		 
-		 if ($conn->query($sql) === TRUE) {
-    	 	#echo "New record created successfully";
-		 } else {
-    	 	echo "Error: " . $sql . "<br>" . $conn->error;
-		 }
-		 
-		 $conn->close();
-		 echo '<br>';
-		}
-		
-		$t = date("H");
- 		
- 		if ($t < "10") {
- 			echo "$name Have a good day!";
- 		}
- 		else if ($t >= "10" && $t < "20") {
- 			echo "$name Good Day!";
- 		}
- 		else {
- 			echo "$name Have a good night!";
- 		}
-	?>
+  	<button type="button" id="btnWeather" onClick=weather()>Weather</button>
+	<button type="button" id="btnThree" onClick=packing()>Packing List</button>
+  	<div id="output">waiting for action</div>
  	<br><br><br>
  	<?php 
  		echo "Web server not yet configured<br>"; 
