f8ce04e0596ab8c35e6e7d3040fc624afca22df9
rdincher <Ryan@Ryans-MacBook-Air.local>
2018-05-06 20:34:00 -0500

    add turn off

--- Lights/lights.php
+++ Lights/lights.php
@@ -16,29 +16,19 @@
   		});
   		return false;
 	}
-	function lightOn() {
-   		
-   		$.ajax({
-     		url:'lightsON.php',
-     		type: "POST",
-     		dataType:'json', // add json datatype to get json
-     		data: ({num: 1}),
-      		complete: function (response) {
-          		$('#search').html(response.responseText);
-      		},
-      		error: function () {
-         		$('#search').html('Bummer: there was an error!');
-     		}
-  		});
-  		return false;
-	}
   </script>
   <script language="Javascript">
   	function lightsOn()
   	{
   		var http = new XMLHttpRequest();
-    	http.open('PUT', "http://192.168.2.5/api/NNNcEcM5gL2AN7EnHzXbyLQkSzg6kwAF0y83DGip/lights/1/state", true);
+    	http.open('PUT', "http://192.168.2.5/api/NNNcEcM5gL2AN7EnHzXbyLQkSzg6kwAF0y83DGip/lights/"+$('#lightNum').val()+"/state", true);
     	http.send('{"on":true}');
+    }
+    function lightsOff()
+  	{
+  		var http = new XMLHttpRequest();
+    	http.open('PUT', "http://192.168.2.5/api/NNNcEcM5gL2AN7EnHzXbyLQkSzg6kwAF0y83DGip/lights/"+$('#lightNum').val()+"/state", true);
+    	http.send('{"on":false}');
     }
   </script>
   </head>
@@ -48,8 +38,9 @@
 	<div id="output"></div>
 	</br>
 	<div id="search">
-  		Light ID: <input id="lightNum" type="number" value="" min="1" style="width: 3em"/>
-  		<button type="button" id="toggle" onClick="lightsOn()">Turn On Light</button>
+  		Light ID: <input id="lightNum" type="number" value="" min="1" style="width: 3em"/></br>
+  		<button type="button" id="toggle" onClick="lightsOn()">Turn On Light</button></br>
+  		<button type="button" id="toggle" onClick="lightsOff()">Turn Off Light</button>
 	</div>
   </body>
 </html>
